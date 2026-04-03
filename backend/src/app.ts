3e390e0c90a974329603437c0b9dac2534ab816f
abdouHZ12 <abdouHZ12@users.noreply.github.com>
2026-04-03 05:05:08 +0100

    added CRUD Document

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,13 @@
 import express, {type Express} from 'express';
 import { errorHandler } from "./middlewares/errorHandler.js";
-
+import DocumentRouter from './routes/DocumentRoute.js'
 const app: Express = express();
 
 app.use(express.json());
 
 // Routes
 
+app.use('/',DocumentRouter)
 // Global error handler (should be after routes)
 app.use(errorHandler);
 
