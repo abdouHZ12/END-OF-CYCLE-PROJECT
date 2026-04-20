26c44d9155f25270ff814e1fdcf1fdafd097790b
kasbadji <kasbadjihalim01@gmail.com>
2026-04-20 15:27:17 +0100

    feat: the backend can handle employee multiple role

--- backend/src/modules/auth/token.service.ts
+++ backend/src/modules/auth/token.service.ts
@@ -1,38 +1,38 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import { config } from '../../config/config.js';
+import { RoleType } from '../../../generated/prisma/client.js';
 
 export const tokenService = {
+  generateAccessToken(employeeId: number, roles: RoleType[]): string {
+    return jwt.sign(
+      { id: employeeId, roles },
+      config.jwt.accessSecret,
+      { expiresIn: config.jwt.accessExpires },
+    );
+  },
 
-    generateAccessToken(employeeId: number, role: string): string {
-        return jwt.sign(
-            { id: employeeId, role},
-            config.jwt.accessSecret,
-            { expiresIn: config.jwt.accessExpires },
-        );
-    },
+  generateRefreshToken(employeeId: number): string {
+    return jwt.sign(
+      { id: employeeId },
+      config.jwt.refreshSecret,
+      { expiresIn: config.jwt.refreshExpires }
+    );
+  },
 
-    generateRefreshToken(employeeId: number): string {
-        return jwt.sign(
-            { id: employeeId },
-            config.jwt.refreshSecret,
-            { expiresIn: config.jwt.refreshExpires }
-        );
-    },
+  verifyRefreshToken(token: string): { id: number } {
+    return jwt.verify(token, config.jwt.refreshSecret) as { id: number };
+  },
 
-    verifyRefreshToken(token: string): { id: number } {
-        return jwt.verify(token, config.jwt.refreshSecret) as { id: number };
-    },
+  verifyAccessToken(token: string): { id: number; roles: RoleType[] } {
+    return jwt.verify(token, config.jwt.accessSecret) as { id: number; roles: RoleType[] };
+  },
 
-    verifyAccessToken(token: string): { id: number; role: string } {
-        return jwt.verify(token, config.jwt.accessSecret) as { id: number; role: string };
-    },
+  async hashToken(token: string): Promise<string> {
+    return bcrypt.hash(token, 10);
+  },
 
-    async hashToken(token: string): Promise<string> {
-        return bcrypt.hash(token, 10);
-    },
-
-    async compareToken(raw: string, hashed: string): Promise<boolean> {
-        return bcrypt.compare(raw, hashed);
-    },
-}
+  async compareToken(raw: string, hashed: string): Promise<boolean> {
+    return bcrypt.compare(raw, hashed);
+  },
+};
