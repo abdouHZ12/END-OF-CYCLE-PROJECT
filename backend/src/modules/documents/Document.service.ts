b9d96c43f8f79bc0f6be2ff733aa8a471a960e83
kasbadji <kasbadjihalim01@gmail.com>
2026-05-03 08:34:31 +0100

    fix: agent removed from the web

--- backend/src/modules/documents/Document.service.ts
+++
@@ -1,5 +0,0 @@
-export * from './services/documentCrud.service.js';
-export * from './services/documentManager.service.js';
-export * from './services/documentPdf.service.js';
-export * from './services/documentScan.service.js';
-export * from './services/documentDss.service.js';
